f522cb5270d1a075de0c2a7b06bc70e1c8cccb90
Daniel Muñoz <djmv225@gmail.com>
2026-07-15 15:55:04 -0500

    feature: add reactivity to todos and setup solid cable

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -10,4 +10,8 @@
 
 class Todo < ApplicationRecord
   validates_presence_of :description
+
+  after_create_commit -> { broadcast_append_to "todos" }
+  after_update_commit -> { broadcast_replace_to "todos" }
+  after_destroy_commit -> { broadcast_remove_to "todos" }
 end
